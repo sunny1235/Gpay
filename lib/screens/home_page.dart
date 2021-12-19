edc2ed9dc03c2c5e9125f1d7db148ece0ee8e57e
sunny <sunny.gupta@neosoftmail.com>
2021-12-19 15:02:46 +0530

    final

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -15,132 +15,174 @@ class _HomePageState extends State<HomePage> {
   final _formKey = GlobalKey<FormState>();
    String? amount = '' ;
     String? name = '';
+    String? number = '';
  
     AudioCache player = AudioCache();
    
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+
       appBar: AppBar(
         backgroundColor: Colors.blueAccent[400],
         centerTitle: true,
         title: const Text('Gpay'),
       ),
-      body: Container(
-        margin: const EdgeInsets.only(top: 100),
-        child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                FittedBox(
-                  fit : BoxFit.fitWidth,
-                  child: const Text(
-                    'Enter Amount',
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400),
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Padding(
-                  padding:  EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.height * 0.15),
-                  child: TextFormField(
-                  //  initialValue: '00',
-                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                     onChanged:(value){
-                       setState(() {
-                         amount = value;
-                       });
-                    } ,
-
-                    validator: (val) {
-                      if (val!.isEmpty) {
-                        return 'Enter Amount please';
-                      }
-                      return null;
-                    },
-                    cursorColor: Colors.black,
-                    decoration: const InputDecoration(
-                      errorStyle: TextStyle(fontSize: 16),
-                      hintText: '00',
-                      prefixText: '\u{20B9}',
-                      
-                    ),
-                    keyboardType: TextInputType.number,
-                    textAlign: TextAlign.start,
-                    style: const TextStyle(
-                        fontSize: 50, fontWeight: FontWeight.w500),
-                  ),
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25),
-                  child: const Text('To get correct name use orginal google pay scanner',style: TextStyle(color: Colors.green,fontSize: 16,fontWeight: FontWeight.w500),),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: TextFormField(
-                    onChanged:(value){
-                        setState(() {
-                          name = value;
-                        });
-                    } ,
-                    validator: (val) {
-                      if (val!.isEmpty) {
-                        return 'Please enter shop name';
-                      }
-                      return null;
-                    },
-                    style: const TextStyle(
-                        fontSize: 22, fontWeight: FontWeight.w500),
-                    decoration:  InputDecoration(
-                        errorStyle: TextStyle(fontSize: 16),
-                        labelText: 'Enter Shop Name',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20)
-                        )),
-                  ),
-                ),
-                 SizedBox(height: MediaQuery.of(context).size.height * 0.08),
-                ElevatedButton(
-                  
-                  style: ElevatedButton.styleFrom(
-                    
-                    primary: Colors.blueAccent[400],
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)
-                    )
-                    ,
-                    elevation: 10,
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 20, horizontal: 15),
-                  ),
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      player.play('gpay.mp3');
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => Payment(amount:amount ,shopName: name,)));
-                    }
-                  },
-                  child: FittedBox(
-                    fit: BoxFit.fitWidth,
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Container(
+          margin: const EdgeInsets.only(top: 100),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  FittedBox(
+                    fit : BoxFit.fitWidth,
                     child: const Text(
-                      'Show Payment',
-                      style: TextStyle(
-                          fontSize: 25, fontWeight: FontWeight.w400),
+                      'Enter Amount',
+                      style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400),
                     ),
                   ),
-                )
-              ],
-            )),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding:  EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.height * 0.15),
+                    child: TextFormField(
+                      maxLength: 5,
+                    //  initialValue: '00',
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                       onChanged:(value){
+                         setState(() {
+                           amount = value;
+                         });
+                      } ,
+      
+                      validator: (val) {
+                        if (val!.isEmpty) {
+                          return 'Enter Amount please';
+                        }
+                        return null;
+                      },
+                      cursorColor: Colors.black,
+                      decoration: const InputDecoration(
+                        errorStyle: TextStyle(fontSize: 16),
+                        hintText: '00',
+                        prefixText: '\u{20B9}',
+                        
+                      ),
+                      keyboardType: TextInputType.number,
+                      textAlign: TextAlign.start,
+                      style: const TextStyle(
+                          fontSize: 50, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25),
+                    child:  Text("To get correct shop's name use orginal google pay scanner",style: TextStyle(color: Colors.grey[600],fontSize: 16,fontWeight: FontWeight.w500),),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Padding(
+                      padding:  EdgeInsets.only(bottom:  MediaQuery.of(context).viewInsets.bottom),
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            onChanged:(value){
+                                setState(() {
+                                  name = value;
+                                });
+                            } ,
+                            validator: (val) {
+                              if (val!.isEmpty) {
+                                return 'Please enter shop name';
+                              }
+                              return null;
+                            },
+                            style: const TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.w500),
+                            decoration:  InputDecoration(
+                                errorStyle: TextStyle(fontSize: 16),
+                                labelText: 'Enter shop name',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20)
+                                )),
+                          ),
+                                SizedBox(height: 20,),
+                           TextFormField(
+                             keyboardType: TextInputType.number,
+                             maxLength: 10,
+                            onChanged:(value){
+                                setState(() {
+                                  number = value;
+                                });
+                            } ,
+                            validator: (val) {
+                              if (val!.isEmpty) {
+                                return 'Please enter mobile number';
+                              }
+                              return null;
+                            },
+                            style: const TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.w500),
+                            decoration:  InputDecoration(
+                                errorStyle: TextStyle(fontSize: 16),
+                                labelText: 'Enter your mobile number',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20)
+                                )),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+
+
+                  
+                 
+                   SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                  ElevatedButton(
+                    
+                    style: ElevatedButton.styleFrom(
+                      
+                      primary: Colors.blueAccent[400],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)
+                      )
+                      ,
+                      elevation: 10,
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 20, horizontal: 15),
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        player.play('gpay.mp3');
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Payment(amount:amount ,shopName: name,number: number,)));
+                      }
+                    },
+                    child: FittedBox(
+                      fit: BoxFit.fitWidth,
+                      child: const Text(
+                        'Show Payment',
+                        style: TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.w400),
+                      ),
+                    ),
+                  )
+                ],
+              )),
+        ),
       ),
     );
   }
--- lib/screens/payment.dart
+++ lib/screens/payment.dart
@@ -8,93 +8,102 @@ class Payment extends StatelessWidget {
 
   final String? amount;
   final String? shopName;
+  final String? number;
 
  final String date = DateFormat.yMMMd().format(DateTime.now());
  final String time = DateFormat.jm().format(DateTime.now());
 
-  Payment({this.amount, this.shopName});
+  Payment({this.amount, this.shopName,this.number});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding:
-            EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.30),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle, color: Colors.blueAccent[400]),
-              height: 130,
-              width: 130,
-              child: const Icon(
-                Icons.check,
-                color: Colors.white,
-                size: 75,
-              ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.08,
-            ),
-
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 50),
-                child: Column(
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Container(
+          // color: Colors.red,
+          height: 900,
+          child: Padding(
+            padding: EdgeInsets.only(top:  MediaQuery.of(context).size.height * 0.30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.blueAccent[400]),
+                  height: 130,
+                  width: 130,
+                  child: const Icon(
+                    Icons.check,
+                    color: Colors.white,
+                    size: 75,
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.08,
+                ),
+                
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 50),
+                    child: Column(
                       children: [
-                        Text(
-                          '\u{20B9}',
-                          style: TextStyle(fontSize: 50),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '\u{20B9}',
+                              style: TextStyle(fontSize: 50),
+                            ),
+                            FittedBox(
+                              child: Text(
+                                '$amount',
+                                style: TextStyle(
+                                  fontSize: 50,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                        Text(
-                          '$amount',
-                          style: TextStyle(
-                            fontSize: 50,
-                            fontWeight: FontWeight.w500,
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.02,
+                        ),
+                        FittedBox(child: Text('Paid to $shopName', style: TextStyle(fontSize: 22))),
+                        FittedBox(child: Text('$number @okbizaxis', style: TextStyle(fontSize: 20))),
+
+                        SizedBox(height: MediaQuery.of(context).size.height * 0.10),
+                        Text('$date $time', style: TextStyle(fontSize: 18)),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.02,
+                        ),
+                        TextButton(
+                          
+                          style: TextButton.styleFrom(
+                            primary: Colors.blueAccent[700],
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                width:1,
+                                color: Colors.grey,
+                                style: BorderStyle.solid
+                              ),
+                              borderRadius: BorderRadius.circular(20)
+                            )
                           ),
-                        ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          }, 
+                          child: Text('Got it')
+                          )
                       ],
                     ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.02,
-                    ),
-                    Text('Paid to $shopName', style: TextStyle(fontSize: 22)),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.10),
-                    Text('$date $time', style: TextStyle(fontSize: 18)),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.02,
-                    ),
-                    TextButton(
-                      
-                      style: TextButton.styleFrom(
-                        primary: Colors.blueAccent[700],
-                        shape: RoundedRectangleBorder(
-                          side: BorderSide(
-                            width:1,
-                            color: Colors.grey,
-                            style: BorderStyle.solid
-                          ),
-                          borderRadius: BorderRadius.circular(20)
-                        )
-                      ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      }, 
-                      child: Text('Got it')
-                      )
-                  ],
+                  ),
                 ),
-              ),
+               
+              ],
             ),
-            //  const SizedBox(
-            //     height: 10,
-            //   ),
-          ],
+          ),
         ),
       ),
     );
